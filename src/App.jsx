c425b2f2d8bd4762605d93245ad35cf814046ab5
Fabian <burner.afire@gmail.com>
2023-06-11 11:34:47 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+import { products as initialProducts } from "./mocks/products.json";
+import { Products } from "./components/Products";
+import { Header } from "./components/Header";
+import { useState } from "react";
+
+function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: "all",
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === "all" || filters.category === product.category)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
+
+  return (
+    <>
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
+    </>
+  );
+}
+
+export default App;
